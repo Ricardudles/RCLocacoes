19c2c54e8ae8ceef1aa7675924d7afef2357b706
Caroline Reinato <cr.reinato@gmail.com>
2022-12-08 07:40:43 -0300

    Exemplo de modelBuilder para mapear relacções e chaves de entidades

--- RCLocacoes.Infra.Data/Context/ApplicationContext.cs
+++ RCLocacoes.Infra.Data/Context/ApplicationContext.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using RCLocacoes.Domain.Entities;
+using RCLocacoes.Infra.Data.Entity.AddressMap;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Numerics;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,56 +17,25 @@ namespace RCLocacoes.Infra.Data.Context
     {
         public ApplicationContext(DbContextOptions options) : base(options) { }
 
-        //public DbSet<Product> Products { get; set; }
-        //public DbSet<Category> Categories { get; set; }
-        //public DbSet<ProductCategory> ProductCategories { get; set; }
-        //public DbSet<Local> Locals { get; set; }
-        //public DbSet<Address> Addresses { get; set; }
-        //public DbSet<Client> Clients { get; set; }
-        //public DbSet<ClientType> ClientTypes { get; set; }
-        //public DbSet<Order> Orders { get; set; }
-        //public DbSet<OrderProduct> OrderProducts { get; set; }
-        //public DbSet<Status> Statuses { get; set; }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            MapEntity(modelBuilder);
+            DesableDeleteCascade(modelBuilder);
+
             base.OnModelCreating(modelBuilder);
+        }
 
-            modelBuilder.Entity<Product>().HasKey(p => p.Id);
-            modelBuilder.Entity<Product>().Property(p => p.Name).HasColumnType("varchar(100)");
-            modelBuilder.Entity<Product>().Property(p => p.Description).HasColumnType("varchar(250)");
-            modelBuilder.Entity<Product>().Property(p => p.RentPrice).HasColumnType("decimal(18,2)").IsRequired();
-            modelBuilder.Entity<Product>().Property(p => p.ReplacementCost).HasColumnType("decimal(18,2)").IsRequired();
-            modelBuilder.Entity<Product>().Property(p => p.Inactive).HasColumnType("bit").IsRequired();
-            modelBuilder.Entity<Product>().Property(p => p.Picture).HasColumnType("varchar(MAX)");
+        private void MapEntity(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
 
-            modelBuilder.Entity<Category>().HasKey(p => p.Id);
-            modelBuilder.Entity<Category>().Property(p => p.Name).HasColumnType("varchar(100)");
-
-            modelBuilder.Entity<ProductCategory>().HasKey(p => p.Id);
-
-            modelBuilder.Entity<Local>().HasKey(p => p.Id);
-
-
-            modelBuilder.Entity<Address>().HasKey(p => p.Id);
-
-
-            modelBuilder.Entity<Client>().HasKey(p => p.Id);
-
-
-            modelBuilder.Entity<ClientType>().HasKey(p => p.Id);
-
-
-            modelBuilder.Entity<Order>().HasKey(p => p.Id);
-
-
-            modelBuilder.Entity<OrderProduct>().HasKey(p => new { p.ProductId, p.OrderId });
-            modelBuilder.Entity<OrderProduct>().Property(p => p.RentPrice).HasColumnType("decimal(18,2)").IsRequired();
-            modelBuilder.Entity<OrderProduct>().Property(p => p.ReplacementCost).HasColumnType("decimal(18,2)").IsRequired();
-
-            modelBuilder.Entity<Status>().HasKey(p => p.Id);
-
-
+        private void DesableDeleteCascade(ModelBuilder modelBuilder)
+        {
+            foreach (var foreignKey in modelBuilder.Model.GetEntityTypes().SelectMany(t => t.GetForeignKeys()))
+            {
+                foreignKey.DeleteBehavior = DeleteBehavior.Restrict;
+            }
         }
     }
 
---
+++ RCLocacoes.Infra.Data/Entity/AddressMap/AddressMap.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RCLocacoes.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RCLocacoes.Infra.Data.Entity.AddressMap
+{
+    public class AddressMap : IEntityTypeConfiguration<Domain.Entities.Address>
+    {
+        public void Configure(EntityTypeBuilder<Address> builder)
+        {
+            builder.ToTable("Address");
+
+            //Exemplo:
+            //builder.HasIndex(p => p.Hash);
+            //builder.HasIndex(p => p.RegistroAcademico).IsUnique();
+
+            //builder.Property(p => p.Nome).IsRequired();
+            //builder.Property(p => p.NomeSocial).IsRequired();
+            //builder.Property(p => p.SexoId).IsRequired();
+
+            //builder.Property(p => p.EscolaId).IsRequired(false);
+
+            //builder.HasOne(p => p.Sexo)
+            //    .WithMany(f => f.Alunos)
+            //    .HasForeignKey(p => p.SexoId);
+
+            //builder.HasOne(p => p.Usuario)
+            //    .WithMany(f => f.Alunos)
+            //    .HasForeignKey(p => p.UsuarioId);
+
+            //builder.HasMany(p => p.AlunoContatos)
+            //    .WithOne(f => f.Aluno)
+            //    .HasForeignKey(f => f.AlunoId);
+        }
+    }
+}
